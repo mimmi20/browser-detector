7ddfce15a1109b9eff8802ecccb5b642e34b2600
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2013-01-20 12:36:18 +0000

    Fehler beheben

--- library/Browscap/Detector/BrowserHandler.php
+++ library/Browscap/Detector/BrowserHandler.php
@@ -305,20 +305,6 @@ abstract class BrowserHandler implements MatcherInterface
     }
     
     /**
-     * returns null, if the browser does not have a specific rendering engine
-     * returns the Engine Handler otherwise
-     *
-     * @return null|\Browscap\Os\Handler
-     */
-    public function detectEngine()
-    {
-        $handler = new \Browscap\Detector\Engine\Unknown();
-        $handler->setUseragent($this->_useragent);
-        
-        return $handler->detect();
-    }
-    
-    /**
      * returns null, if the device does not have a specific Operating System
      * returns the OS Handler otherwise
      *
