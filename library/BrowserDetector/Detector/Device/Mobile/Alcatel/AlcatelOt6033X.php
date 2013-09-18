3ff4d9f884b0352ef3c4fbbc6310918e2ca2713d
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2013-09-18 07:37:06 +0200

    corrected size of the device

--- library/BrowserDetector/Detector/Device/Mobile/Alcatel/AlcatelOt6033X.php
+++ library/BrowserDetector/Detector/Device/Mobile/Alcatel/AlcatelOt6033X.php
@@ -91,7 +91,7 @@ class AlcatelOt6033X
             'manufacturer_name' => new Company\Alcatel(),
             'brand_name' => new Company\Alcatel(),
             'model_extra_info'          => null,
-            'marketing_name'            => 'One Touch Star 6033X',
+            'marketing_name'            => 'One Touch Idol Ultra',
             'has_qwerty_keyboard'       => true,
             'pointing_method'           => 'touchscreen',
             'device_bits'               => null, // not in wurfl
@@ -112,8 +112,8 @@ class AlcatelOt6033X
             'rows'                   => null,
             'max_image_width'        => null,
             'max_image_height'       => null,
-            'resolution_width'       => 480,
-            'resolution_height'      => 800,
+            'resolution_width'       => 720,
+            'resolution_height'      => 1280,
             'dual_orientation'       => true,
             'colors'                 => 16777216,
             
