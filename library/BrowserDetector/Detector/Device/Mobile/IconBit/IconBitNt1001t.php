346154b3ab1060f78506c41a9cea14b6d9fbccf4
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2013-09-18 17:22:50 +0200

    added new devices iconBIT NT-1001T and iconBIT NT-1002T

---
+++ library/BrowserDetector/Detector/Device/Mobile/IconBit/IconBitNt1001t.php
@@ -0,0 +1,199 @@
+<?php
+namespace BrowserDetector\Detector\Device\Mobile\IconBit;
+
+/**
+ * PHP version 5.3
+ *
+ * LICENSE:
+ *
+ * Copyright (c) 2013, Thomas Mueller <t_mueller_stolzenhain@yahoo.de>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the authors nor the names of its contributors may be 
+ *   used to endorse or promote products derived from this software without 
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  BrowserDetector
+ * @package   BrowserDetector
+ * @copyright 2012-2013 Thomas Mueller
+ * @license   http://opensource.org/licenses/BSD-3-Clause New BSD License
+ * @version   SVN: $Id$
+ */
+
+use \BrowserDetector\Detector\DeviceHandler;
+use \BrowserDetector\Helper\Utils;
+use \BrowserDetector\Detector\MatcherInterface;
+use \BrowserDetector\Detector\MatcherInterface\DeviceInterface;
+use \BrowserDetector\Detector\BrowserHandler;
+use \BrowserDetector\Detector\EngineHandler;
+use \BrowserDetector\Detector\OsHandler;
+use \BrowserDetector\Detector\Version;
+use \BrowserDetector\Detector\Company;
+use \BrowserDetector\Detector\Type\Device as DeviceType;
+
+/**
+ * @category  BrowserDetector
+ * @package   BrowserDetector
+ * @copyright 2012-2013 Thomas Mueller
+ * @license   http://opensource.org/licenses/BSD-3-Clause New BSD License
+ * @version   SVN: $Id$
+ */
+class IconBitNt1001t
+    extends DeviceHandler
+    implements MatcherInterface, DeviceInterface
+{
+    /**
+     * the detected browser properties
+     *
+     * @var array
+     */
+    protected $properties = array();
+    
+    /**
+     * Class Constructor
+     *
+     * @return DeviceHandler
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        
+        $this->properties = array(
+            'wurflKey' => null, // not in wurfl
+            
+            // kind of device
+            'device_type' => new DeviceType\Tablet(), // not in wurfl
+            
+            // device
+            'model_name'                => 'NT-1001T',
+            'model_version'             => null, // not in wurfl
+            'manufacturer_name' => new Company\IconBit(),
+            'brand_name' => new Company\IconBit(),
+            'model_extra_info'          => null,
+            'marketing_name'            => 'THOR LE NT-1001T',
+            'has_qwerty_keyboard'       => true,
+            'pointing_method'           => 'touchscreen',
+            'device_bits'               => null, // not in wurfl
+            'device_cpu'                => null, // not in wurfl
+            
+            // product info
+            'can_assign_phone_number'   => false,
+            'ununiqueness_handler'      => null,
+            'uaprof'                    => null,
+            'uaprof2'                   => null,
+            'uaprof3'                   => null,
+            'unique'                    => true,
+            
+            // display
+            'physical_screen_width'  => null,
+            'physical_screen_height' => null,
+            'columns'                => null,
+            'rows'                   => null,
+            'max_image_width'        => null,
+            'max_image_height'       => null,
+            'resolution_width'       => 1024,
+            'resolution_height'      => 600,
+            'dual_orientation'       => true,
+            'colors'                 => 65500,
+            
+            // sms
+            'sms_enabled' => false,
+            
+            // chips
+            'nfc_support' => false,
+        );
+    }
+    
+    /**
+     * checks if this device is able to handle the useragent
+     *
+     * @return boolean returns TRUE, if this device can handle the useragent
+     */
+    public function canHandle()
+    {
+        if (!$this->utils->checkIfContains('nt-1001t', true)) {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    /**
+     * gets the weight of the handler, which is used for sorting
+     *
+     * @return integer
+     */
+    public function getWeight()
+    {
+        return 3;
+    }
+    
+    /**
+     * detects the device name from the given user agent
+     *
+     * @param string $userAgent
+     *
+     * @return StdClass
+     */
+    public function detectDevice()
+    {
+        return $this;
+    }
+    
+    /**
+     * returns null, if the device does not have a specific Browser
+     * returns the Browser Handler otherwise
+     *
+     * @return null|\BrowserDetector\Os\Handler
+     */
+    public function detectBrowser()
+    {
+        $browsers = array(
+            new \BrowserDetector\Detector\Browser\Mobile\Android(),
+            new \BrowserDetector\Detector\Browser\Mobile\Chrome(),
+            new \BrowserDetector\Detector\Browser\Mobile\Dalvik()
+        );
+        
+        $chain = new \BrowserDetector\Detector\Chain();
+        $chain->setUserAgent($this->_useragent);
+        $chain->setHandlers($browsers);
+        $chain->setDefaultHandler(new \BrowserDetector\Detector\Browser\Unknown());
+        
+        return $chain->detect();
+    }
+    
+    /**
+     * returns null, if the device does not have a specific Operating System
+     * returns the OS Handler otherwise
+     *
+     * @return null|\BrowserDetector\Os\Handler
+     */
+    public function detectOs()
+    {
+        $handler = new \BrowserDetector\Detector\Os\Android();
+        $handler->setUseragent($this->_useragent);
+        
+        return $handler->detect();
+    }
+}
