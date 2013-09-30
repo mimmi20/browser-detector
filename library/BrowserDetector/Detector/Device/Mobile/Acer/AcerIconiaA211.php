91c531f1afd77862f510af5b009d1500c13e113d
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2013-09-30 17:28:49 +0200

    changed device type for Acer Iconia A211

--- library/BrowserDetector/Detector/Device/Mobile/Acer/AcerIconiaA211.php
+++ library/BrowserDetector/Detector/Device/Mobile/Acer/AcerIconiaA211.php
@@ -83,7 +83,7 @@ class AcerIconiaA211
             'wurflKey' => 'acer_iconia_tab_a211_ver1', // not in wurfl
             
             // kind of device
-            'device_type' => new DeviceType\Tablet(), // not in wurfl
+            'device_type' => new DeviceType\FonePad(), // not in wurfl
             
             // device
             'model_name'                => 'A211',
