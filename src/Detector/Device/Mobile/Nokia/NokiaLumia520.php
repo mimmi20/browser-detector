a3b2e54caffa50e4e3a85c60d59f377325f8cc02
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-05-12 07:34:14 +0200

    add Nokia Lumia 520

---
+++ src/Detector/Device/Mobile/Nokia/NokiaLumia520.php
@@ -0,0 +1,174 @@
+<?php
+namespace BrowserDetector\Detector\Device\Mobile\Nokia;
+
+/**
+ * PHP version 5.3
+ *
+ * LICENSE:
+ *
+ * Copyright (c) 2013, Thomas Mueller <t_mueller_stolzenhain@yahoo.de>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the authors nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  BrowserDetector
+ * @package   BrowserDetector
+ * @copyright 2012-2013 Thomas Mueller
+ * @license   http://opensource.org/licenses/BSD-3-Clause New BSD License
+ * @version   SVN: $Id$
+ */
+
+use BrowserDetector\Detector\BrowserHandler;
+use BrowserDetector\Detector\Company;
+use BrowserDetector\Detector\DeviceHandler;
+use BrowserDetector\Detector\EngineHandler;
+use BrowserDetector\Detector\MatcherInterface;
+use BrowserDetector\Detector\MatcherInterface\DeviceInterface;
+use BrowserDetector\Detector\Os\WindowsPhoneOs;
+use BrowserDetector\Detector\OsHandler;
+use BrowserDetector\Detector\Type\Device as DeviceType;
+
+/**
+ * @category  BrowserDetector
+ * @package   BrowserDetector
+ * @copyright 2012-2013 Thomas Mueller
+ * @license   http://opensource.org/licenses/BSD-3-Clause New BSD License
+ * @version   SVN: $Id$
+ */
+class NokiaLumia520
+    extends DeviceHandler
+    implements MatcherInterface, DeviceInterface
+{
+    /**
+     * the detected browser properties
+     *
+     * @var array
+     */
+    protected $properties = array();
+
+    /**
+     * Class Constructor
+     *
+     * @return \BrowserDetector\Detector\Device\Mobile\Nokia\NokiaLumia520
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->properties = array(
+            'wurflKey'                => null, // not in wurfl
+
+            // kind of device
+            'device_type'             => new DeviceType\MobilePhone(), // not in wurfl
+
+            // device
+            'model_name'              => 'Lumia 520',
+            'model_version'           => null, // not in wurfl
+            'manufacturer_name'       => new Company\Nokia(),
+            'brand_name'              => new Company\Nokia(),
+            'model_extra_info'        => null,
+            'marketing_name'          => 'Lumia 520',
+            'has_qwerty_keyboard'     => true,
+            'pointing_method'         => 'touchscreen',
+            'device_bits'             => null, // not in wurfl
+            'device_cpu'              => null, // not in wurfl
+
+            // product info
+            'can_assign_phone_number' => true,
+            'ununiqueness_handler'    => null,
+            'uaprof'                  => null,
+            'uaprof2'                 => null,
+            'uaprof3'                 => null,
+            'unique'                  => true,
+
+            // display
+            'physical_screen_width'   => 41,
+            'physical_screen_height'  => 89,
+            'columns'                 => 12,
+            'rows'                    => 20,
+            'max_image_width'         => 320,
+            'max_image_height'        => 480,
+            'resolution_width'        => 480,
+            'resolution_height'       => 800,
+            'dual_orientation'        => true,
+            'colors'                  => 65536,
+
+            // sms
+            'sms_enabled'             => true,
+
+            // chips
+            'nfc_support'             => true,
+        );
+    }
+
+    /**
+     * checks if this device is able to handle the useragent
+     *
+     * @return boolean returns TRUE, if this device can handle the useragent
+     */
+    public function canHandle()
+    {
+        if (!$this->utils->checkIfContains('nokia; lumia 520', true)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * gets the weight of the handler, which is used for sorting
+     *
+     * @return integer
+     */
+    public function getWeight()
+    {
+        return 3;
+    }
+
+    /**
+     * detects the device name from the given user agent
+     *
+     * @return \BrowserDetector\Detector\Device\Mobile\Nokia\NokiaLumia520
+     */
+    public function detectDevice()
+    {
+        return $this;
+    }
+
+    /**
+     * returns null, if the device does not have a specific Operating System
+     * returns the OS Handler otherwise
+     *
+     * @return \BrowserDetector\Detector\Os\WindowsPhoneOs
+     */
+    public function detectOs()
+    {
+        $handler = new WindowsPhoneOs();
+        $handler->setUseragent($this->_useragent);
+
+        return $handler->detect();
+    }
+}
