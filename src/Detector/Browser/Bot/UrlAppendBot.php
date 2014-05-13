70d716fb66fd56f6d83f7498c02e301e393cbfe6
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-05-13 07:33:08 +0200

    add URLAppendBot

---
+++ src/Detector/Browser/Bot/UrlAppendBot.php
@@ -0,0 +1,163 @@
+<?php
+namespace BrowserDetector\Detector\Browser\Bot;
+
+/**
+ * PHP version 5.3
+ *
+ * LICENSE:
+ *
+ * Copyright (c) 2013, Thomas Mueller <t_mueller_stolzenhain@yahoo.de>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the authors nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  BrowserDetector
+ * @package   BrowserDetector
+ * @copyright 2012-2013 Thomas Mueller
+ * @license   http://opensource.org/licenses/BSD-3-Clause New BSD License
+ * @version   SVN: $Id$
+ */
+
+use BrowserDetector\Detector\BrowserHandler;
+use BrowserDetector\Detector\Company;
+use BrowserDetector\Detector\MatcherInterface;
+use BrowserDetector\Detector\MatcherInterface\BrowserInterface;
+use BrowserDetector\Detector\Type\Browser as BrowserType;
+use BrowserDetector\Detector\Version;
+use BrowserDetector\Detector\Engine\UnknownEngine;
+
+/**
+ * @category  BrowserDetector
+ * @package   BrowserDetector
+ * @copyright 2012-2013 Thomas Mueller
+ * @license   http://opensource.org/licenses/BSD-3-Clause New BSD License
+ * @version   SVN: $Id$
+ */
+class UrlAppendBot
+    extends BrowserHandler
+    implements MatcherInterface, BrowserInterface
+{
+    /**
+     * the detected browser properties
+     *
+     * @var array
+     */
+    protected $properties = array();
+
+    /**
+     * Class Constructor
+     *
+     * @return BrowserHandler
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->properties = array(
+            // kind of device
+            'browser_type'                 => new BrowserType\Bot(), // not in wurfl
+
+            // browser
+            'mobile_browser'               => 'URLAppendBot',
+            'mobile_browser_version'       => null,
+            'mobile_browser_bits'          => null, // not in wurfl
+            'mobile_browser_manufacturer'  => new Company\ProfoundNetworks(), // not in wurfl
+            'mobile_browser_modus'         => null, // not in wurfl
+
+            // product info
+            'can_skip_aligned_link_row'    => false,
+            'device_claims_web_support'    => false,
+
+            // pdf
+            'pdf_support'                  => true,
+
+            // bugs
+            'empty_option_value_support'   => true,
+            'basic_authentication_support' => true,
+            'post_method_support'          => true,
+
+            // rss
+            'rss_support'                  => false,
+        );
+    }
+
+    /**
+     * Returns true if this handler can handle the given user agent
+     *
+     * @return bool
+     */
+    public function canHandle()
+    {
+        if (!$this->utils->checkIfContains(array('URLAppendBot'))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * detects the browser version from the given user agent
+     *
+     * @return string
+     */
+    protected function _detectVersion()
+    {
+        $detector = new Version();
+        $detector->setUserAgent($this->useragent);
+
+        $searches = array('URLAppendBot');
+
+        $this->setCapability(
+            'mobile_browser_version', $detector->detectVersion($searches)
+        );
+
+        return $this;
+    }
+
+    /**
+     * gets the weight of the handler, which is used for sorting
+     *
+     * @return integer
+     */
+    public function getWeight()
+    {
+        return 19073;
+    }
+
+    /**
+     * returns null, if the device does not have a specific Operating System
+     * returns the OS Handler otherwise
+     *
+     * @return null|\BrowserDetector\Detector\OsHandler
+     */
+    public function detectEngine()
+    {
+        $handler = new UnknownEngine();
+        $handler->setUseragent($this->useragent);
+
+        return $handler->detect();
+    }
+}
