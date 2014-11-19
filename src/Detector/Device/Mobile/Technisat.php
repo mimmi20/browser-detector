8dbd23368348740820ff86b9d0ce7760df7698ed
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-11-19 16:26:34 +0100

    add TechniSat TechniPad 10 3G

---
+++ src/Detector/Device/Mobile/Technisat.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * Copyright (c) 2012-2014, Thomas Mueller <t_mueller_stolzenhain@yahoo.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @category  BrowserDetector
+ * @package   BrowserDetector
+ * @author    Thomas Mueller <t_mueller_stolzenhain@yahoo.de>
+ * @copyright 2012-2014 Thomas Mueller
+ * @license   http://www.opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/mimmi20/BrowserDetector
+ */
+
+namespace BrowserDetector\Detector\Device\Mobile;
+
+use BrowserDetector\Detector\Chain;
+use BrowserDetector\Detector\Company;
+use BrowserDetector\Detector\DeviceHandler;
+use BrowserDetector\Detector\MatcherInterface\MatcherInterface;
+use BrowserDetector\Detector\MatcherInterface\DeviceInterface;
+use BrowserDetector\Detector\MatcherInterface\DeviceHasChildrenInterface;
+use BrowserDetector\Detector\Os\AndroidOs;
+use BrowserDetector\Detector\Os\Java;
+use BrowserDetector\Detector\Os\Symbianos;
+use BrowserDetector\Detector\Os\UnknownOs;
+use BrowserDetector\Detector\Os\WindowsMobileOs;
+use BrowserDetector\Detector\Os\WindowsPhoneOs;
+use BrowserDetector\Detector\Type\Device as DeviceType;
+
+/**
+ * @category  BrowserDetector
+ * @package   BrowserDetector
+ * @copyright 2012-2014 Thomas Mueller
+ * @license   http://www.opensource.org/licenses/MIT MIT License
+ */
+class Technisat
+    extends DeviceHandler
+    implements DeviceInterface, DeviceHasChildrenInterface
+{
+    /**
+     * the detected browser properties
+     *
+     * @var array
+     */
+    protected $properties = array(
+        'wurflKey'                => null, // not in wurfl
+
+        // device
+        'model_name'              => 'general Technisat Device',
+        'model_extra_info'        => null,
+        'marketing_name'          => 'general Technisat Device',
+        'has_qwerty_keyboard'     => true,
+        'pointing_method'         => 'touchscreen',
+
+        // product info
+        'ununiqueness_handler'    => null,
+        'uaprof'                  => null,
+        'uaprof2'                 => null,
+        'uaprof3'                 => null,
+        'unique'                  => true,
+
+        // display
+        'physical_screen_width'   => null,
+        'physical_screen_height'  => null,
+        'columns'                 => null,
+        'rows'                    => null,
+        'max_image_width'         => null,
+        'max_image_height'        => null,
+        'resolution_width'        => null,
+        'resolution_height'       => null,
+        'dual_orientation'        => null,
+        'colors'                  => null,
+
+        // sms
+        'sms_enabled'             => true,
+
+        // chips
+        'nfc_support'             => true,
+    );
+
+    /**
+     * checks if this device is able to handle the useragent
+     *
+     * @return boolean returns TRUE, if this device can handle the useragent
+     */
+    public function canHandle()
+    {
+        $phones = array(
+            'Technisat',
+            'TechniPad'
+        );
+
+        if (!$this->utils->checkIfContains($phones, true)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * gets the weight of the handler, which is used for sorting
+     *
+     * @return integer
+     */
+    public function getWeight()
+    {
+        return 3;
+    }
+
+    /**
+     * returns the type of the current device
+     *
+     * @return \BrowserDetector\Detector\Type\Device\TypeInterface
+     */
+    public function getDeviceType()
+    {
+        return new DeviceType\MobilePhone();
+    }
+
+    /**
+     * returns the type of the current device
+     *
+     * @return \BrowserDetector\Detector\Company\CompanyInterface
+     */
+    public function getManufacturer()
+    {
+        return new Company\TechniSat();
+    }
+
+    /**
+     * returns the type of the current device
+     *
+     * @return \BrowserDetector\Detector\Company\CompanyInterface
+     */
+    public function getBrand()
+    {
+        return new Company\TechniSat();
+    }
+
+    /**
+     * detects the device name from the given user agent
+     *
+     * @return \BrowserDetector\Detector\DeviceHandler
+     */
+    public function detectDevice()
+    {
+        $chain = new Chain();
+        $chain->setUserAgent($this->_useragent);
+        $chain->setNamespace(__NAMESPACE__ . '\\Technisat');
+        $chain->setDirectory(
+            __DIR__ . DIRECTORY_SEPARATOR . 'Technisat' . DIRECTORY_SEPARATOR
+        );
+        $chain->setDefaultHandler($this);
+
+        return $chain->detect();
+    }
+
+    /**
+     * returns null, if the device does not have a specific Operating System, returns the OS Handler otherwise
+     *
+     * @return \BrowserDetector\Detector\OsHandler
+     */
+    public function detectOs()
+    {
+        $os = array(
+            new AndroidOs(),
+            new Java(),
+            new Symbianos(),
+            new WindowsMobileOs(),
+            new WindowsPhoneOs()
+        );
+
+        $chain = new Chain();
+        $chain->setDefaultHandler(new UnknownOs());
+        $chain->setUseragent($this->_useragent);
+        $chain->setHandlers($os);
+
+        return $chain->detect();
+    }
+}
