57b9c2cb135f70ae7e0a0309abfc7b69649d0bf5
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2015-02-26 07:21:45 +0100

    add new wurfl key

--- src/Detector/Device/Mobile/Samsung/SamsungGtp5210.php
+++ src/Detector/Device/Mobile/Samsung/SamsungGtp5210.php
@@ -175,6 +175,55 @@ class SamsungGtp5210
         $engine->setCapability('xhtml_send_mms_string', 'mms:');
         $engine->setCapability('xhtml_send_sms_string', 'sms:');
 
+        $osVersion = $os->detectVersion()->getVersion(
+            Version::MAJORMINOR
+        );
+
+        if (2.3 == $osVersion) {
+            $engine->setCapability('xhtml_can_embed_video', 'play_and_stop');
+        }
+
+        switch ($browser->getName()) {
+        case 'Android Webkit':
+            switch ((float)$osVersion) {
+            case 4.4:
+                $this->setCapability('wurflKey', 'samsung_gt_p5200_ver1_suban44p5210');
+                break;
+            case 2.1:
+            case 2.2:
+            case 2.3:
+            case 3.1:
+            case 3.2:
+            case 4.0:
+            case 4.1:
+            case 4.2:
+            default:
+                // nothing to do here
+                break;
+            }
+            break;
+        case 'Chrome':
+            $engine->setCapability('is_sencha_touch_ok', false);
+
+            switch ((float)$osVersion) {
+            case 2.1:
+            case 2.2:
+            case 2.3:
+            case 3.1:
+            case 3.2:
+            case 4.0:
+            case 4.1:
+            case 4.2:
+            default:
+                // nothing to do here
+                break;
+            }
+            break;
+        default:
+            // nothing to do here
+            break;
+        }
+
         return $this;
     }
 }
