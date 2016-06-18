b1655da7903be7e277bb828c79b74b2c761ec195
Thomas Müller <mimmi20@live.de>
2016-06-18 15:08:58 +0200

    add tests

---
+++ tests/issues/browscap-issue-784.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'browscap-issue-784' => [
+        'ua'         => 'Chrome/15.0.860.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/15.0.860.0',
+        'properties' => [
+            'Browser_Name'            => 'Default Browser',
+            'Browser_Type'            => 'unknown',
+            'Browser_Bits'            => 0,
+            'Browser_Maker'           => 'unknown',
+            'Browser_Modus'           => 'unknown',
+            'Browser_Version'         => '0.0',
+            'Platform_Name'           => 'Windows',
+            'Platform_Version'        => 'Vista.0.0',
+            'Platform_Bits'           => 32,
+            'Platform_Maker'          => 'Microsoft Corporation',
+            'Device_Name'             => 'unknown',
+            'Device_Maker'            => 'unknown',
+            'Device_Type'             => 'unknown',
+            'Device_Pointing_Method'  => 'unknown',
+            'Device_Code_Name'        => 'unknown',
+            'Device_Brand_Name'       => 'unknown',
+            'RenderingEngine_Name'    => 'unknown',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker'   => 'unknown',
+        ],
+    ],
+];
