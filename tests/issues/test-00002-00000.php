142b310b78bbbdaac8468f91862453a2c6b156ec
Thomas Müller <mimmi20@live.de>
2016-07-02 16:15:23 +0200

    remove duplicate tests

--- tests/issues/test-00002-00000.php
+++ tests/issues/test-00002-00000.php
@@ -1,30 +1,6 @@
 <?php
 
 return [
-    'test-0002-1' => [
-            'ua'         => 'Mozilla/0.91 Beta (Windows)',
-            'properties' => [
-                'Browser_Name'            => 'Default Browser',
-                'Browser_Type'            => 'unknown',
-                'Browser_Bits'            => 32,
-                'Browser_Maker'           => 'unknown',
-                'Browser_Modus'           => 'unknown',
-                'Browser_Version'         => '0.0',
-                'Platform_Name'           => 'Windows',
-                'Platform_Version'        => '0.0.0',
-                'Platform_Bits'           => 32,
-                'Platform_Maker'          => 'Microsoft Corporation',
-                'Device_Name'             => 'unknown',
-                'Device_Maker'            => 'unknown',
-                'Device_Type'             => 'unknown',
-                'Device_Pointing_Method'  => 'unknown',
-                'Device_Code_Name'        => 'unknown',
-                'Device_Brand_Name'       => 'unknown',
-                'RenderingEngine_Name'    => 'unknown',
-                'RenderingEngine_Version' => 'unknown',
-                'RenderingEngine_Maker'   => 'unknown',
-            ],
-        ],
     'test-0002-2' => [
             'ua'         => 'QuickTime\\xaa.7.0.4 (qtver=7.0.4;cpu=PPC;os=Mac 10.3.9)',
             'properties' => [
