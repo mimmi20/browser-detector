e99761450873503b58a6bfe388e37ed9267ec5cf
Thomas Müller <mimmi20@live.de>
2016-07-26 07:02:34 +0200

    add Gfive factory

--- src/Detector/Factory/Device/MobileFactory.php
+++ src/Detector/Factory/Device/MobileFactory.php
@@ -611,8 +611,10 @@ class MobileFactory implements FactoryInterface
         }
 
         if (preg_match('/gfive/i', $useragent)) {
-            $device = new Gfive($useragent, []);
-        } elseif (preg_match('/(HP|P160U|TouchPad|Pixi|palm|Blazer|cm_tenderloin)/i', $useragent)) {
+            return Mobile\GfiveFactory::detect($useragent);
+        }
+
+        if (preg_match('/(HP|P160U|TouchPad|Pixi|palm|Blazer|cm_tenderloin)/i', $useragent)) {
             $device = new Hp($useragent, []);
         } elseif (preg_match('/iconbit/i', $useragent)) {
             $device = new IconBit($useragent, []);
