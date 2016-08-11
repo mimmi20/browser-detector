50f08a2fac0dac148af631285b38e740f55db416
Thomas Müller <mimmi20@live.de>
2016-08-11 23:17:37 +0200

    add new tests

---
+++ tests/issues/browscap-issue-01032.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'browscap-issue-1032' => [
+        'ua'         => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2794.0 Safari/537.36',
+        'properties' => [
+            'Browser_Name'            => 'Chrome',
+            'Browser_Type'            => 'Browser',
+            'Browser_Bits'            => 64,
+            'Browser_Maker'           => 'Google Inc',
+            'Browser_Modus'           => 'unknown',
+            'Browser_Version'         => '54.0',
+            'Platform_Name'           => 'Win10',
+            'Platform_Version'        => '10.0',
+            'Platform_Bits'           => 64,
+            'Platform_Maker'          => 'Microsoft Corporation',
+            'Device_Name'             => 'Windows Desktop',
+            'Device_Maker'            => 'Various',
+            'Device_Type'             => 'Desktop',
+            'Device_Pointing_Method'  => 'mouse',
+            'Device_Code_Name'        => 'Windows Desktop',
+            'Device_Brand_Name'       => 'unknown',
+            'RenderingEngine_Name'    => 'Blink',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker'   => 'Google Inc',
+        ],
+    ],
+];
