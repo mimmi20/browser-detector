1738e40c88ad894a6ab2e26680e443b47815d808
Thomas Müller <mimmi20@live.de>
2016-08-12 21:47:52 +0200

    add platform script

---
+++ convert-os.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thomas Müller
+ * Date: 03.03.2016
+ * Time: 07:22
+ */
+
+chdir(__DIR__);
+
+require 'vendor/autoload.php';
+
+ini_set('memory_limit', '-1');
+
+$sourceDirectory = 'src\\Detector\\Os\\';
+
+$iterator = new \RecursiveDirectoryIterator($sourceDirectory);
+
+foreach (new \RecursiveIteratorIterator($iterator) as $file) {
+    /** @var $file \SplFileInfo */
+    if (!$file->isFile() || $file->getExtension() !== 'php') {
+        continue;
+    }
+
+    $fullpath    = $file->getPathname();
+    $pathMatches = [];
+
+    $filecontent      = file_get_contents($fullpath);
+    $marketingMatches = [];
+
+    if (preg_match('/\\$this\\-\\>manufacturer = \\(new Company\\\\([^\\;]+)/', $filecontent, $marketingMatches)) {
+        $manufacturerFull = $marketingMatches[1];
+    } else {
+        continue;
+    }
+
+    echo 'processing ', $fullpath, PHP_EOL;
+
+    $marketingMatches = [];
+
+    if (preg_match('/\\$this\\-\\>manufacturer = \\(new Company\\\\([^\\(]+)/', $filecontent, $marketingMatches)) {
+        $manufacturer = $marketingMatches[1];
+    } else {
+        $manufacturer = 'Unknown';
+    }
+
+    $templateContent = str_replace(
+        $manufacturerFull,
+        'CompanyFactory::get(\'' . $manufacturer . '\')->getName()',
+        $filecontent
+    );
+
+    file_put_contents($fullpath, $templateContent);
+
+    if (false !== strpos($templateContent, '#')) {
+        echo 'placeholders found in file ', $fullpath, PHP_EOL;
+        exit;
+    }
+    //exit;
+}
