9c815d6d4ccffa576edaac4356c300a6a7f090d7
Thomas Müller <mimmi20@live.de>
2016-09-22 21:12:34 +0200

    add new tests

--- tests/issues/00133/test-00133-00021.php
+++ tests/issues/00133/test-00133-00021.php
@@ -1,33 +1,6 @@
 <?php
 
 return [
-    'test-00133-02102' => [
-        'ua'         => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2750.0 Iron Safari/537.36',
-        'properties' => [
-            'Browser_Name'            => 'Iron',
-            'Browser_Type'            => 'Browser',
-            'Browser_Bits'            => 32,
-            'Browser_Maker'           => 'unknown',
-            'Browser_Modus'           => 'unknown',
-            'Browser_Version'         => '0.0',
-            'Platform_Codename'       => 'Windows NT 6.1',
-            'Platform_Marketingname'  => 'Windows 7',
-            'Platform_Version'        => '0.0.0',
-            'Platform_Bits'           => 64,
-            'Platform_Maker'          => 'Microsoft Corporation',
-            'Platform_Brand_Name'     => 'Microsoft',
-            'Device_Name'             => 'Windows Desktop',
-            'Device_Maker'            => 'unknown',
-            'Device_Type'             => 'Desktop',
-            'Device_Pointing_Method'  => 'mouse',
-            'Device_Dual_Orientation' => false,
-            'Device_Code_Name'        => 'Windows Desktop',
-            'Device_Brand_Name'       => 'unknown',
-            'RenderingEngine_Name'    => 'Blink',
-            'RenderingEngine_Version' => 'unknown',
-            'RenderingEngine_Maker'   => 'Google Inc',
-        ],
-    ],
     'test-00133-02106' => [
         'ua'         => 'Mozilla/5.0 (Windows NT 6.0; rv:47.0) Gecko/20100101 Firefox/47.0,gzip(gfe)',
         'properties' => [
