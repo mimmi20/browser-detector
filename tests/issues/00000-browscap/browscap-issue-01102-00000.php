f02ab9c98d5766365f18477ac5b1a6a130a1590f
Thomas Müller <mimmi20@live.de>
2016-12-16 22:01:46 +0100

    add new tests

---
+++ tests/issues/00000-browscap/browscap-issue-01102-00000.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'browscap-issue-1102' => [
+        'ua'         => 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:7.0.1) Gecko/20151105 Postbox/4.0.8',
+        'properties' => [
+            'Browser_Name'            => 'Postbox',
+            'Browser_Type'            => 'Email Client',
+            'Browser_Bits'            => 32,
+            'Browser_Maker'           => 'Postbox, Inc.',
+            'Browser_Modus'           => 'unknown',
+            'Browser_Version'         => '4.0',
+            'Platform_Codename'       => 'Windows NT 6.2',
+            'Platform_Marketingname'  => 'Windows 8',
+            'Platform_Version'        => '0.0.0',
+            'Platform_Bits'           => 64,
+            'Platform_Maker'          => 'Microsoft Corporation',
+            'Platform_Brand_Name'     => 'Microsoft Corporation',
+            'Device_Name'             => 'Windows Desktop',
+            'Device_Maker'            => 'Various',
+            'Device_Type'             => 'Desktop',
+            'Device_Pointing_Method'  => 'mouse',
+            'Device_Dual_Orientation' => false,
+            'Device_Code_Name'        => 'Windows Desktop',
+            'Device_Brand_Name'       => 'unknown',
+            'RenderingEngine_Name'    => 'Gecko',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker'   => 'Mozilla Foundation',
+        ],
+    ],
+];
