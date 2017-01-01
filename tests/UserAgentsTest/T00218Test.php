31da01b62ff6e9cf766d08539bdc89742cf3706f
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2017-01-01 22:46:26 +0100

    update tests

---
+++ tests/UserAgentsTest/T00218Test.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright (c) 2012-2017 Thomas Mueller
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Refer to the LICENSE file distributed with this package.
+ *
+ * @category   CompareTest
+ *
+ * @copyright 2012-2017 Thomas Mueller
+ * @license   http://www.opensource.org/licenses/MIT MIT License
+ */
+
+namespace BrowserDetectorTest\UserAgentsTest;
+
+use BrowserDetectorTest\UserAgentsTest;
+
+/**
+ * Class UserAgentsTest
+ *
+ * @category   CompareTest
+ *
+ * @author     Thomas Mueller <mimmi20@live.de>
+ * @group      useragenttest
+ */
+class T00218Test extends UserAgentsTest
+{
+    /**
+     * @var string
+     */
+    protected $sourceDirectory = 'tests/issues/00218/';
+
+    /**
+     * @dataProvider userAgentDataProvider
+     *
+     * @param string $userAgent
+     * @param array  $expectedProperties
+     *
+     * @throws \Exception
+     * @group  integration
+     * @group  useragenttest
+     * @group  00218
+     */
+    public function testUserAgents($userAgent, $expectedProperties)
+    {
+        if (!is_array($expectedProperties) || !count($expectedProperties)) {
+            self::markTestSkipped('Could not run test - no properties were defined to test');
+        }
+
+        parent::testUserAgents($userAgent, $expectedProperties);
+    }
+}
