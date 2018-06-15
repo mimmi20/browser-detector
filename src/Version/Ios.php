9d99e89b999a0149445b17789f498b13a7789b9d
Thomas Müller <mimmi20@live.de>
2018-06-15 23:50:51 +0200

    add new devices and tests

--- src/Version/Ios.php
+++ src/Version/Ios.php
@@ -52,9 +52,62 @@ class Ios implements VersionCacheFactoryInterface
             }
         }
 
+        if (false !== mb_stripos($useragent, 'darwin')) {
+            $searches = [
+                '/darwin\/18/i' => '12.0',
+                '/darwin\/17\.7/i' => '11.4',
+                '/darwin\/17\.6/i' => '11.4',
+                '/cfnetwork\/897(\.\d+)? darwin\/17\.5/i' => '11.3',
+                '/darwin\/17\.4/i' => '11.2',
+                '/darwin\/17\.3/i' => '11.2',
+                '/darwin\/17\.2/i' => '11.1',
+                '/darwin\/17/i' => '11.0',
+                '/darwin\/16\.7/i' => '10.3.3',
+                '/darwin\/16\.6/i' => '10.3.2',
+                '/darwin\/16\.5/i' => '10.3',
+                '/darwin\/16\.3/i' => '10.2',
+                '/darwin\/16\.1/i' => '10.1',
+                '/darwin\/16/i' => '10.0',
+                '/darwin\/15\.6/i' => '9.3.3',
+                '/darwin\/15\.5/i' => '9.3.2',
+                '/darwin\/15\.4/i' => '9.3',
+                '/cfnetwork\/758\.3(\.\d+)? darwin\/15/i' => '9.3',
+                '/cfnetwork\/758\.2(\.\d+)? darwin\/15/i' => '9.2',
+                '/cfnetwork\/758\.1(\.\d+)? darwin\/15/i' => '9.1',
+                '/darwin\/15/i' => '9.0',
+                '/cfnetwork\/711\.[45](\.\d+)? darwin\/14/i' => '8.4',
+                '/cfnetwork\/711\.3(\.\d+)? darwin\/14/i' => '8.3',
+                '/cfnetwork\/711\.2(\.\d+)? darwin\/14/i' => '8.2',
+                '/cfnetwork\/711\.1(\.\d+)? darwin\/14/i' => '8.1',
+                '/cfnetwork\/711([\.\d]+)? darwin\/14/i' => '8.0',
+                '/cfnetwork\/709(\.\d+)? darwin\/14/i' => '8.0',
+                '/cfnetwork\/672\.1(\.\d+)? darwin\/14/i' => '7.1',
+                '/darwin\/14/i' => '7.0',
+                '/cfnetwork\/609\.1(\.\d+)? darwin\/13/i' => '6.1',
+                '/darwin\/13/i' => '6.0',
+                '/cfnetwork\/548\.1(\.\d+)? darwin\/11/i' => '5.1',
+                '/cfnetwork\/548([\.\d]+)? darwin\/11/i' => '5.0',
+                '/darwin\/11/i' => '4.3',
+                '/darwin\/10\.4/i' => '4.2',
+                '/cfnetwork\/485\.10(\.\d+)? darwin\/10\.3/i' => '4.1',
+                '/cfnetwork\/485\.2(\.\d+)? darwin\/10\.3/i' => '4.0',
+                '/darwin\/10\.3/i' => '3.2',
+                '/cfnetwork\/459 darwin\/10/i' => '3.1',
+                '/darwin\/10/i' => '3.0',
+                '/darwin\/9\.4/i' => '2.1',
+                '/darwin\/9\.3/i' => '2.0',
+                '/darwin\/9/i' => '1.0',
+            ];
+
+            foreach ($searches as $rule => $version) {
+                if (preg_match($rule, $useragent)) {
+                    return (new VersionFactory())->set($version);
+                }
+            }
+        }
+
         if (false !== mb_stripos($useragent, 'cfnetwork')) {
             $searches = [
-                '/cfnetwork\/897/i' => '11.0',
                 '/cfnetwork\/887/i' => '11.0',
                 '/cfnetwork\/808\.2/i' => '10.2',
                 '/cfnetwork\/808\.1/i' => '10.1',
@@ -84,44 +137,6 @@ class Ios implements VersionCacheFactoryInterface
                 '/cfnetwork\/485\.2/i' => '4.0',
                 '/cfnetwork\/467\.12/i' => '3.2',
                 '/cfnetwork\/459/i' => '3.1',
-            ];
-
-            foreach ($searches as $rule => $version) {
-                if (preg_match($rule, $useragent)) {
-                    return (new VersionFactory())->set($version);
-                }
-            }
-        }
-
-        if (false !== mb_stripos($useragent, 'darwin')) {
-            $searches = [
-                '/darwin\/18/i' => '12.0',
-                '/darwin\/17\.7/i' => '11.4',
-                '/darwin\/17\.6/i' => '11.4',
-                '/darwin\/17\.5/i' => '11.3',
-                '/darwin\/17\.4/i' => '11.2',
-                '/darwin\/17\.3/i' => '11.2',
-                '/darwin\/17\.2/i' => '11.1',
-                '/darwin\/17/i' => '11.0',
-                '/darwin\/16\.7/i' => '10.3.3',
-                '/darwin\/16\.6/i' => '10.3.2',
-                '/darwin\/16\.5/i' => '10.3',
-                '/darwin\/16\.3/i' => '10.2',
-                '/darwin\/16\.1/i' => '10.1',
-                '/darwin\/16/i' => '10.0',
-                '/darwin\/15\.6/i' => '9.3.3',
-                '/darwin\/15\.5/i' => '9.3.2',
-                '/darwin\/15\.4/i' => '9.3',
-                '/darwin\/15/i' => '9.0',
-                '/darwin\/14/i' => '7.0',
-                '/darwin\/13/i' => '6.0',
-                '/darwin\/11/i' => '4.3',
-                '/darwin\/10\.4/i' => '4.2',
-                '/darwin\/10\.3/i' => '3.2',
-                '/darwin\/10/i' => '3.0',
-                '/darwin\/9\.4/i' => '2.1',
-                '/darwin\/9\.3/i' => '2.0',
-                '/darwin\/9/i' => '1.0',
             ];
 
             foreach ($searches as $rule => $version) {
