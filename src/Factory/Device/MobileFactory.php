305f8f55860a16881ccce61ec9d2c88960f3ce87
Thomas Müller <mimmi20@live.de>
2018-08-09 07:18:39 +0200

    add Storex devices

--- src/Factory/Device/MobileFactory.php
+++ src/Factory/Device/MobileFactory.php
@@ -780,6 +780,7 @@ class MobileFactory implements DeviceFactoryInterface
         '/rct\d{4}/i' => 'rca-tablets',
         '/(centurion|gladiator| glory|luxury|sensuelle|victory)([ _\-]?[2-6])?[);\/ ]|surfing tab/i' => 'brondi',
         '/momo(\d|mini)/i' => 'ployer',
+        '/ezee/i' => 'storex',
         '/I5/' => 'sop',
         '/i5/' => 'vsun',
         '/KIN\.(One|Two)|ZuneHD|Windows NT 6\.(2|3).*ARM;/' => 'microsoft',
