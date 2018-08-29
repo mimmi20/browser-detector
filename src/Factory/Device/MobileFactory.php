61b101b5c42c9bb55557210535149dc75a2bed09
Thomas Müller <mimmi20@live.de>
2018-08-29 22:46:52 +0200

    add new Gigaset device

--- src/Factory/Device/MobileFactory.php
+++ src/Factory/Device/MobileFactory.php
@@ -137,7 +137,7 @@ class MobileFactory implements DeviceFactoryInterface
         '/sprint/i' => 'sprint',
         '/gionee/i' => 'gionee',
         '/videocon/i' => 'videocon',
-        '/gigaset|gs55\-6|gs53\-6|gs370_plus/i' => 'gigaset',
+        '/gigaset|gs55\-6|gs53\-6|gs270 plus|gs370_plus/i' => 'gigaset',
         '/(dns|airtab)[ _\-]([^;\/]+)build|s4503q|s4505m/i' => 'dns',
         '/kyocera/i' => 'kyocera',
         '/texet/i' => 'texet',
