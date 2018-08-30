29309a2a76d20f3a6df8c3665cc1c62e85c5f42a
Thomas Müller <mimmi20@live.de>
2018-08-30 23:38:08 +0200

    add new JLinksz device

--- src/Factory/Device/MobileFactory.php
+++ src/Factory/Device/MobileFactory.php
@@ -714,7 +714,7 @@ class MobileFactory implements DeviceFactoryInterface
         '/picasso/i' => 'bluboo',
         '/strongphoneq4/i' => 'evolveo',
         '/shift[457]/i' => 'shift',
-        '/k960/i' => 'jlinksz',
+        '/k960|zh960/i' => 'jlinksz',
         '/i\-call|elektra l|neon[79]/i' => 'ijoy',
         '/ektra/i' => 'kodak',
         '/kt107/i' => 'bdf',
