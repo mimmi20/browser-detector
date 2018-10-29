c948cedd8dfadc3faba5daac442088717bfe211f
Thomas Müller <mimmi20@live.de>
2018-10-29 17:21:58 +0100

    add new ZTE devices

--- src/Factory/Device/MobileFactory.php
+++ src/Factory/Device/MobileFactory.php
@@ -125,7 +125,7 @@ class MobileFactory implements DeviceFactoryInterface
         '/bv[5-9]000|dm550|crown| r6 | a8 |alife [ps]1|omega_pro|a7pro/i' => 'blackview',
         '/karbonn|titanium|machfive|sparkle v|s109|k9 smart grand/i' => 'karbonn', // must be before acer
         '/micromax[ \-\_]?[a-z0-9]+|(p70221|a121|a120|a116|a114|a093|a065| a96| a50| a47|q327)( build|[;\/\)])|mid1014/i' => 'micromax',
-        '/zte|racer|smart ?(tab(10|7)|4g|ultra 6)|orange (hi 4g|reyo)|beeline pro|lutea|bs 451|n9132|grand s flex|e8q\+|s8q|s7q|blade|momodesign md droid|[ \-]a310(?!e)|atlas[_ ]w|base tab|kis plus|n799d|n9101|n9180|n9510|n951[57]|n9520|n9521|n9810|n918st|n958st|nx[0-9]{2,3}|open[c2]|u9180| v9 |v788d|v8000|v9180|x501|x920|z221|z232tl|z233vl?|z717[bv]l|z768g|z820|z83[359]|z855|z851m?|z965|z97[18]|z98[1236](dl)?|z999/i' => 'zte', // must be before orange and acer
+        '/zte|racer|smart ?(tab(10|7)|4g|ultra 6)|orange (hi 4g|reyo)|beeline pro|lutea|bs 451|n9132|grand s flex|e8q\+|s8q|s7q|blade|momodesign md droid|[ \-]a310(?!e)|atlas[_ ]w|base tab|kis plus|n799d|n9101|n9136|n9180|n9510|n951[57]|n9520|n9521|n9810|n918st|n958st|nx[0-9]{2,3}|open[c2]|u9180| v9 |v788d|v8000|v9180|x501|x920|z221|z23[23]|z353vl|z558vl|z71[78]|z768g|z79[289]|z81[25]|z82[08]|z83[13569]|z85[125]|z86[12]|z899vl|z91[67]|z95[58]|z965|z97[18]|z98[1236](dl)?|z999/i' => 'zte', // must be before orange and acer
         '/smart tab 4(?!g)|vfd [0-9]{3}|985n|vodafone smart 4 max|smart 4 turbo|vodafone 975(?!n)|vodacom/i' => 'vodafone',
         '/qmobile|q-smart|qtab| x900 /i' => 'qmobile', // must be before acer
         '/shield tablet/i' => 'nvidia',
