843688eb527da728997781781fb83e3161055e10
Thomas Müller <mimmi20@live.de>
2019-01-08 22:13:29 +0100

    add new devices and tests

---
+++ src/Loader/CompanyLoaderFactoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the browser-detector package.
+ *
+ * Copyright (c) 2012-2019, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace BrowserDetector\Loader;
+
+interface CompanyLoaderFactoryInterface extends SpecificLoaderFactoryInterface
+{
+    /**
+     * @return CompanyLoaderInterface
+     */
+    public function __invoke(): CompanyLoaderInterface;
+}
