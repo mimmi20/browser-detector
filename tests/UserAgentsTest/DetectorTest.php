adf810751a45b8e7a50dbfbf64dde7141ad61fb7
Thomas Müller <thomas.mueller@geld.de>
2021-01-29 16:41:35 +0100

    update test

--- tests/UserAgentsTest/DetectorTest.php
+++ tests/UserAgentsTest/DetectorTest.php
@@ -71,43 +71,133 @@ final class DetectorTest extends TestCase
             ->expects(self::never())
             ->method('emergency');
 
-        $cache = new Class implements CacheInterface
-        {
+        $cache = new class() implements CacheInterface {
+            /**
+             * Fetches a value from the cache.
+             *
+             * @param mixed $key     the unique key of this item in the cache
+             * @param mixed $default default value to return if the key does not exist
+             *
+             * @throws \Psr\SimpleCache\InvalidArgumentException
+             *                                                   MUST be thrown if the $key string is not a legal value
+             *
+             * @return mixed the value of the item from the cache, or $default in case of cache miss
+             */
             public function get($key, $default = null)
             {
                 return null;
             }
 
+            /**
+             * Persists data in the cache, uniquely referenced by a key with an optional expiration TTL time.
+             *
+             * @param mixed                  $key   the key of the item to store
+             * @param mixed                  $value the value of the item to store, must be serializable
+             * @param \DateInterval|int|null $ttl   Optional. The TTL value of this item. If no value is sent and
+             *                                      the driver supports TTL then the library may set a default value
+             *                                      for it or let the driver take care of that.
+             *
+             * @throws \Psr\SimpleCache\InvalidArgumentException
+             *                                                   MUST be thrown if the $key string is not a legal value
+             *
+             * @return bool true on success and false on failure
+             */
             public function set($key, $value, $ttl = null): bool
             {
                 return false;
             }
 
+            /**
+             * Delete an item from the cache by its unique key.
+             *
+             * @param mixed $key the unique cache key of the item to delete
+             *
+             * @throws \Psr\SimpleCache\InvalidArgumentException
+             *                                                   MUST be thrown if the $key string is not a legal value
+             *
+             * @return bool True if the item was successfully removed. False if there was an error.
+             */
             public function delete($key): bool
             {
                 return false;
             }
 
+            /**
+             * Wipes clean the entire cache's keys.
+             *
+             * @return bool true on success and false on failure
+             */
             public function clear(): bool
             {
                 return false;
             }
 
-            public function getMultiple($keys, $default = null):iterable
+            /**
+             * Obtains multiple cache items by their unique keys.
+             *
+             * @param mixed $keys    a list of keys that can obtained in a single operation
+             * @param mixed $default default value to return for keys that do not exist
+             *
+             * @throws \Psr\SimpleCache\InvalidArgumentException MUST be thrown if $keys is neither an array nor a Traversable,
+             *                                                   or if any of the $keys are not a legal value
+             *
+             * @return iterable A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
+             */
+            public function getMultiple($keys, $default = null): iterable
             {
                 return [];
             }
 
+            /**
+             * Persists a set of key => value pairs in the cache, with an optional TTL.
+             *
+             * @param mixed                  $values a list of key => value pairs for a multiple-set operation
+             * @param \DateInterval|int|null $ttl    Optional. The TTL value of this item. If no value is sent and
+             *                                       the driver supports TTL then the library may set a default value
+             *                                       for it or let the driver take care of that.
+             *
+             * @throws \Psr\SimpleCache\InvalidArgumentException
+             *                                                   MUST be thrown if $values is neither an array nor a Traversable,
+             *                                                   or if any of the $values are not a legal value
+             *
+             * @return bool true on success and false on failure
+             */
             public function setMultiple($values, $ttl = null): bool
             {
                 return false;
             }
 
+            /**
+             * Deletes multiple cache items in a single operation.
+             *
+             * @param mixed $keys a list of string-based keys to be deleted
+             *
+             * @throws \Psr\SimpleCache\InvalidArgumentException
+             *                                                   MUST be thrown if $keys is neither an array nor a Traversable,
+             *                                                   or if any of the $keys are not a legal value
+             *
+             * @return bool True if the items were successfully removed. False if there was an error.
+             */
             public function deleteMultiple($keys): bool
             {
                 return false;
             }
 
+            /**
+             * Determines whether an item is present in the cache.
+             *
+             * NOTE: It is recommended that has() is only to be used for cache warming type purposes
+             * and not to be used within your live applications operations for get/set, as this method
+             * is subject to a race condition where your has() will return true and immediately after,
+             * another script can remove it making the state of your app out of date.
+             *
+             * @param mixed $key the cache item key
+             *
+             * @throws \Psr\SimpleCache\InvalidArgumentException
+             *                                                   MUST be thrown if the $key string is not a legal value
+             *
+             * @return bool
+             */
             public function has($key): bool
             {
                 return false;
