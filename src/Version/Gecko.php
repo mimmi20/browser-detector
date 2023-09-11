f34b95aa39e1bab64a8f10dadce43f3dc93c3bbe
Thomas Müller <mimmi20@live.de>
2023-09-11 10:54:23 +0200

    regroup clients

---
+++ src/Version/Gecko.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * This file is part of the browser-detector package.
+ *
+ * Copyright (c) 2012-2023, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace BrowserDetector\Version;
+
+use Psr\Log\LoggerInterface;
+
+use function preg_match;
+
+final class Gecko implements VersionDetectorInterface
+{
+    /** @throws void */
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly VersionFactoryInterface $versionFactory,
+    ) {
+        // nothing to do
+    }
+
+    /**
+     * returns the version of the operating system/platform
+     *
+     * @throws void
+     */
+    public function detectVersion(string $useragent): VersionInterface
+    {
+        $geckoMatches = null;
+        $rvMatches    = null;
+        $ffMatches    = null;
+        $doMatch      = preg_match('/gecko\/(?P<geckoversion>[\d\.]+)/i', $useragent, $geckoMatches);
+
+        if (!$doMatch) {
+            return new NullVersion();
+        }
+
+        preg_match('/rv\\:(?P<rvversion>[\d\.]+)/i', $useragent, $rvMatches);
+        preg_match('/firefox\/(?P<ffversion>[\d\.]+)/i', $useragent, $ffMatches);
+
+        $geckoversion = $geckoMatches['geckoversion'] ?? null;
+        $ffversion    = $ffMatches['ffversion'] ?? null;
+        $rvversion    = $rvMatches['rvversion'] ?? null;
+
+        if (!empty($geckoversion) && !empty($ffversion) && $geckoversion === $ffversion) {
+            try {
+                return $this->versionFactory->set($geckoversion);
+            } catch (NotNumericException $e) {
+                $this->logger->info($e);
+            }
+
+            return new NullVersion();
+        }
+
+        if (!empty($rvversion)) {
+            try {
+                return $this->versionFactory->set($rvversion);
+            } catch (NotNumericException $e) {
+                $this->logger->info($e);
+            }
+        }
+
+        return new NullVersion();
+    }
+}
---
+++ tests/DetectorTest/Version/GeckoFactoryTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file is part of the browser-detector package.
+ *
+ * Copyright (c) 2012-2023, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace BrowserDetectorTest\Version;
+
+use BrowserDetector\Version\Gecko;
+use BrowserDetector\Version\GeckoFactory;
+use PHPUnit\Framework\Exception;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
+
+use function assert;
+use function sprintf;
+
+final class GeckoFactoryTest extends TestCase
+{
+    private GeckoFactory $object;
+
+    /** @throws void */
+    protected function setUp(): void
+    {
+        $this->object = new GeckoFactory();
+    }
+
+    /**
+     * @throws ExpectationFailedException
+     * @throws Exception
+     */
+    public function testInvoke(): void
+    {
+        $object = $this->object;
+        assert(
+            $object instanceof GeckoFactory,
+            sprintf(
+                '$object should be an instance of %s, but is %s',
+                GeckoFactory::class,
+                $object::class,
+            ),
+        );
+        $result = $object(new NullLogger());
+        self::assertInstanceOf(Gecko::class, $result);
+    }
+}
