2730ccc7f76401b0d51e4c62eac1a7c9b2252c30
Thomas Müller <mimmi20@live.de>
2025-06-28 16:56:01 +0200

    add new tests

--- tests/DetectorTest/Loader/DeviceLoaderTest.php
+++ tests/DetectorTest/Loader/DeviceLoaderTest.php
@@ -200,7 +200,7 @@ final class DeviceLoaderTest extends TestCase
      * @throws NoPreviousThrowableException
      * @throws Exception
      */
-    public function testLoadNoVersion(): void
+    public function testLoadWithoutError(): void
     {
         $logger = $this->createMock(LoggerInterface::class);
         $logger
@@ -284,6 +284,354 @@ final class DeviceLoaderTest extends TestCase
             ->method('load')
             ->with('xyz')
             ->willReturn($company);
+
+        $object = new DeviceLoader(logger: $logger, initData: $initData, companyLoader: $companyLoader);
+
+        $result = $object->load('test-key');
+
+        $expected = new Device(
+            deviceName: null,
+            marketingName: null,
+            manufacturer: $company,
+            brand: $company,
+            type: Type::Unknown,
+            display: new Display(720, 1440, true, 7),
+            dualOrientation: false,
+            simCount: 0,
+        );
+
+        self::assertSame($expected->toArray(), $result->getDevice()->toArray());
+        self::assertSame('test-platform', $result->getOs());
+    }
+
+    /**
+     * @throws Exception
+     * @throws NotFoundException
+     * @throws UnexpectedValueException
+     * @throws RuntimeException
+     * @throws ReflectionException
+     * @throws NoPreviousThrowableException
+     * @throws Exception
+     */
+    public function testLoadWithError1(): void
+    {
+        $exception = new NotFoundException('x was not found');
+
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger
+            ->expects(self::once())
+            ->method('info')
+            ->with($exception, []);
+        $logger
+            ->expects(self::never())
+            ->method('notice');
+        $logger
+            ->expects(self::never())
+            ->method('warning');
+        $logger
+            ->expects(self::never())
+            ->method('error');
+        $logger
+            ->expects(self::never())
+            ->method('critical');
+        $logger
+            ->expects(self::never())
+            ->method('alert');
+        $logger
+            ->expects(self::never())
+            ->method('emergency');
+
+        $initData = new DeviceData(
+            strategy: new class () implements StrategyInterface {
+                /**
+                 * @throws void
+                 *
+                 * @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
+                 */
+                #[Override]
+                public function extract(mixed $value, object | null $object = null): null
+                {
+                    return null;
+                }
+
+                /**
+                 * @param array<mixed>|null $data
+                 *
+                 * @return array<string, mixed>
+                 *
+                 * @throws void
+                 *
+                 * @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
+                 */
+                #[Override]
+                public function hydrate(mixed $value, array | null $data): array
+                {
+                    return [];
+                }
+            },
+            company: 'test-company',
+        );
+
+        $platformData = new DataDevice(
+            deviceName: null,
+            marketingName: null,
+            manufacturer: 'xyz',
+            brand: null,
+            type: null,
+            display: [
+                'width' => 720,
+                'height' => 1440,
+                'touch' => true,
+                'size' => 7,
+            ],
+            dualOrientation: false,
+            simCount: 0,
+            platform: 'test-platform',
+        );
+
+        $prop = new ReflectionProperty($initData, 'items');
+        $prop->setValue($initData, ['test-key' => $platformData]);
+
+        $company = new Company(type: 'unknown', name: null, brandname: null);
+
+        $companyLoader = $this->createMock(CompanyLoaderInterface::class);
+        $companyLoader
+            ->expects(self::once())
+            ->method('load')
+            ->with('xyz')
+            ->willThrowException($exception);
+
+        $object = new DeviceLoader(logger: $logger, initData: $initData, companyLoader: $companyLoader);
+
+        $result = $object->load('test-key');
+
+        $expected = new Device(
+            deviceName: null,
+            marketingName: null,
+            manufacturer: $company,
+            brand: $company,
+            type: Type::Unknown,
+            display: new Display(720, 1440, true, 7),
+            dualOrientation: false,
+            simCount: 0,
+        );
+
+        self::assertSame($expected->toArray(), $result->getDevice()->toArray());
+        self::assertSame('test-platform', $result->getOs());
+    }
+
+    /**
+     * @throws Exception
+     * @throws NotFoundException
+     * @throws UnexpectedValueException
+     * @throws RuntimeException
+     * @throws ReflectionException
+     * @throws NoPreviousThrowableException
+     * @throws Exception
+     */
+    public function testLoadWithError2(): void
+    {
+        $exception = new NotFoundException('x was not found');
+
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger
+            ->expects(self::once())
+            ->method('info')
+            ->with($exception, []);
+        $logger
+            ->expects(self::never())
+            ->method('notice');
+        $logger
+            ->expects(self::never())
+            ->method('warning');
+        $logger
+            ->expects(self::never())
+            ->method('error');
+        $logger
+            ->expects(self::never())
+            ->method('critical');
+        $logger
+            ->expects(self::never())
+            ->method('alert');
+        $logger
+            ->expects(self::never())
+            ->method('emergency');
+
+        $initData = new DeviceData(
+            strategy: new class () implements StrategyInterface {
+                /**
+                 * @throws void
+                 *
+                 * @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
+                 */
+                #[Override]
+                public function extract(mixed $value, object | null $object = null): null
+                {
+                    return null;
+                }
+
+                /**
+                 * @param array<mixed>|null $data
+                 *
+                 * @return array<string, mixed>
+                 *
+                 * @throws void
+                 *
+                 * @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
+                 */
+                #[Override]
+                public function hydrate(mixed $value, array | null $data): array
+                {
+                    return [];
+                }
+            },
+            company: 'test-company',
+        );
+
+        $platformData = new DataDevice(
+            deviceName: null,
+            marketingName: null,
+            manufacturer: null,
+            brand: 'xyz',
+            type: null,
+            display: [
+                'width' => 720,
+                'height' => 1440,
+                'touch' => true,
+                'size' => 7,
+            ],
+            dualOrientation: false,
+            simCount: 0,
+            platform: 'test-platform',
+        );
+
+        $prop = new ReflectionProperty($initData, 'items');
+        $prop->setValue($initData, ['test-key' => $platformData]);
+
+        $company = new Company(type: 'unknown', name: null, brandname: null);
+
+        $companyLoader = $this->createMock(CompanyLoaderInterface::class);
+        $companyLoader
+            ->expects(self::once())
+            ->method('load')
+            ->with('xyz')
+            ->willThrowException($exception);
+
+        $object = new DeviceLoader(logger: $logger, initData: $initData, companyLoader: $companyLoader);
+
+        $result = $object->load('test-key');
+
+        $expected = new Device(
+            deviceName: null,
+            marketingName: null,
+            manufacturer: $company,
+            brand: $company,
+            type: Type::Unknown,
+            display: new Display(720, 1440, true, 7),
+            dualOrientation: false,
+            simCount: 0,
+        );
+
+        self::assertSame($expected->toArray(), $result->getDevice()->toArray());
+        self::assertSame('test-platform', $result->getOs());
+    }
+
+    /**
+     * @throws Exception
+     * @throws NotFoundException
+     * @throws UnexpectedValueException
+     * @throws RuntimeException
+     * @throws ReflectionException
+     * @throws NoPreviousThrowableException
+     * @throws Exception
+     */
+    public function testLoadWithError3(): void
+    {
+        $exception = new NotFoundException('x was not found');
+
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger
+            ->expects(self::exactly(2))
+            ->method('info')
+            ->with($exception, []);
+        $logger
+            ->expects(self::never())
+            ->method('notice');
+        $logger
+            ->expects(self::never())
+            ->method('warning');
+        $logger
+            ->expects(self::never())
+            ->method('error');
+        $logger
+            ->expects(self::never())
+            ->method('critical');
+        $logger
+            ->expects(self::never())
+            ->method('alert');
+        $logger
+            ->expects(self::never())
+            ->method('emergency');
+
+        $initData = new DeviceData(
+            strategy: new class () implements StrategyInterface {
+                /**
+                 * @throws void
+                 *
+                 * @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
+                 */
+                #[Override]
+                public function extract(mixed $value, object | null $object = null): null
+                {
+                    return null;
+                }
+
+                /**
+                 * @param array<mixed>|null $data
+                 *
+                 * @return array<string, mixed>
+                 *
+                 * @throws void
+                 *
+                 * @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
+                 */
+                #[Override]
+                public function hydrate(mixed $value, array | null $data): array
+                {
+                    return [];
+                }
+            },
+            company: 'test-company',
+        );
+
+        $platformData = new DataDevice(
+            deviceName: null,
+            marketingName: null,
+            manufacturer: 'xyz',
+            brand: 'xyz',
+            type: null,
+            display: [
+                'width' => 720,
+                'height' => 1440,
+                'touch' => true,
+                'size' => 7,
+            ],
+            dualOrientation: false,
+            simCount: 0,
+            platform: 'test-platform',
+        );
+
+        $prop = new ReflectionProperty($initData, 'items');
+        $prop->setValue($initData, ['test-key' => $platformData]);
+
+        $company = new Company(type: 'unknown', name: null, brandname: null);
+
+        $companyLoader = $this->createMock(CompanyLoaderInterface::class);
+        $companyLoader
+            ->expects(self::exactly(2))
+            ->method('load')
+            ->with('xyz')
+            ->willThrowException($exception);
 
         $object = new DeviceLoader(logger: $logger, initData: $initData, companyLoader: $companyLoader);
 
