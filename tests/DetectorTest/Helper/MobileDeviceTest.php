85dab5c170d10c228032224222e944c7a3e53abe
Thomas Müller <mimmi20@live.de>
2026-05-18 07:17:15 +0200

    add new Apple and Samsung devices

--- tests/DetectorTest/Helper/MobileDeviceTest.php
+++ tests/DetectorTest/Helper/MobileDeviceTest.php
@@ -192,6 +192,7 @@ final class MobileDeviceTest extends TestCase
             ['Mitsu/1.2.B (MT560)'],
             ['KONKA V7 KWAP1.2'],
             ['Mozilla/5.0 (X11; U; Linux i686; Slackware/13.0; ar;) AppleWebKit/530  (OpenSSL/0.9.8l; compatible; KHTML/4.3.2; Smart 2x2; Remote 2x2; rv:1.9.0.15) Gecko/20090412 KDDI-CA33 UP.Browser/6.2.0.10.4 MMP/2.0'],
+            ['SoftBank/1.0/821SC/SCJ001/SN000000000000000 Flash/Flash-Lite/2.0'],
         ];
     }
 
