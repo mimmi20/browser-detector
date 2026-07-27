8fa5713cae4779d24aab5a17a7c961697ff42404
Thomas Müller <mimmi20@live.de>
2026-07-27 21:10:26 +0200

    fix namespaces in tests

--- tests/DetectorTest/Data/EngineTest.php
+++ tests/DetectorTest/Data/EngineTest.php
@@ -11,7 +11,7 @@
 
 declare(strict_types = 1);
 
-namespace Data;
+namespace BrowserDetectorTest\Data;
 
 use BrowserDetector\Data\Company;
 use BrowserDetector\Data\Engine;
--- tests/DetectorTest/Parser/Header/UseragentEngineVersionTest.php
+++ tests/DetectorTest/Parser/Header/UseragentEngineVersionTest.php
@@ -11,7 +11,7 @@
 
 declare(strict_types = 1);
 
-namespace Parser\Header;
+namespace BrowserDetectorTest\Parser\Header;
 
 use BrowserDetector\Data\Engine;
 use BrowserDetector\Parser\Header\SetVersionTrait;
